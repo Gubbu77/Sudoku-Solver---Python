81c83aeb27fcf68075b6ebc9d26a16cd0b3f9cbd
Indrajith.vs <91895505+Gubbu77@users.noreply.github.com>
2023-01-28 21:52:20 +0530

    Update solver.py

--- solver.py
+++ solver.py
@@ -8,6 +8,8 @@ grid = [[0, 6, 4, 0, 0, 8, 0, 0, 0],
         [4, 7, 8, 0, 5, 9, 0, 0, 0],
         [2, 5, 9, 7, 0, 3, 0, 0, 0]]
 
+
+# main program to solve the sudoku
 def solve(a):
 
     find = find_empty(a)
