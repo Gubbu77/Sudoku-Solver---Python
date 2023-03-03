267666aca1539773f9a4d80803486346d511b77a
Indrajith.vs <91895505+Gubbu77@users.noreply.github.com>
2023-03-03 11:37:57 +0530

    Update solver.py

--- solver.py
+++ solver.py
@@ -8,28 +8,21 @@ grid = [[0, 6, 4, 0, 0, 8, 0, 0, 0],
         [4, 7, 8, 0, 5, 9, 0, 0, 0],
         [2, 5, 9, 7, 0, 3, 0, 0, 0]]
 
-
 # main program to solve the sudoku
 def solve(a):
-
     find = find_empty(a)
     if not find:
         return True
     else:
         row, col = find
-
     for i in range(1, 10):
         if valid(a, i, (row, col)):
             a[row][col] = i
 
             if solve(a):
                 return True
-
             a[row][col] = 0
-
     return False
-
-
 # finding the valid row and column
 def valid(a, num, pos):
     #check row
@@ -40,7 +33,6 @@ def valid(a, num, pos):
     for i in range(len(a)):
         if a[i][pos[1]] == num and pos[0] != i:
             return False
-
     #check box
     box_x = pos[1] // 3
     box_y = pos[0] // 3
@@ -59,7 +51,6 @@ def puzzle(a):
         for j in range(len(a[0])):
             if j % 3 == 0 and j != 0 :
                 print(" | ", end= " ")
-
             if j == 8 :
                 print(a[i][j])
             else :
@@ -72,8 +63,6 @@ def find_empty(a):
             if a[i][j] == 0:
                 return (i, j)
     return None
-
-
 
 puzzle(grid)
 solve(grid)
